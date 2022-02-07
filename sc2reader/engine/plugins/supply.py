7aedf6dd62a4a3cd9ac7d600d2f60320cb82b5aa
Daniel Fulton <fulton.d.p@gmail.com>
2022-02-07 00:48:18 -0800

    Completely rewrote plugin to capture more data.
    Did away with time offset projections for supply, instead capturing
    more UnitEvents and BasicCommandEvents in order to completely track
    production, unit, and structure queues.
    
    This is an in-progress version, but demonstrates the new method now
    properly captures Zerg supply mechanics including larva, drones to
    buildings, and egg and building cancellation.
    
    Additional To-Dos:
      - handle evolution events (e.g. banes, ravs, lurkers, broods)
      - handle unit transformations that change types (e.g. hellbat/hellion)
      - implement supply/supply capacity summaries
      - events are currently tracked by frame, but convert to time rep
      - search in time
      - handle redundant units (e.g. CreepTumor, CreepTumorQueen)
      - diagnose unhandled to-larva UnitTypeChangeEvents
      - handle all Terran/Protoss events
      - test cases?

--- sc2reader/engine/plugins/supply.py
+++ sc2reader/engine/plugins/supply.py
@@ -1,186 +1,302 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals, division
 
+from dataclasses import dataclass
 from collections import defaultdict
+from enum import Enum
 
+class SupplyType(Enum):
+    WORKER = 1
+    ARMY = 2
+    CAPACITY = 3
 
+@dataclass
+class UnitSupply:
+    """ Data structure to hold information about a single unit's supply."""
+    build_time: int
+    supply: float
+    supply_type: SupplyType = SupplyType.ARMY      
+
+class ActivePlayerSupply:
+    def __init__(self):
+        self.preproduction = {}
+        self.production = {}
+        self.units = {}
+        self.structures = {}
+        
+    def get_current_supply(self):
+        workers = 0
+        army = 0
+        capacity = 0
+        #for uid,unit_supply in active_units.:
+        
+class PlayerSupply:
+    """ Data structure to hold supply information. """
+    
+    def __init__(self):
+        """ Create a new _Supply instance. """
+        self.data = [(0,0,0,0)] # list of tuples [(time,workers,army,capacity)]
+        self.production = [(0,{})] # 
+        self.units = [(0,{})] # 
+        self.structures = [(0,{})] # 
+        #self.supply_capped_real_seconds = 0
+
+    def add(self,event_time,workers=0,army=0,capacity=0):
+        latest_supply = self.data[-1][1:]
+        new_supply = tuple(map(lambda x,y:x+y, latest_supply,(workers,army,capacity)))
+        self.data.append( (event_time,)+new_supply )
+        
+    def sync(self,frame,cursup):
+        if not isinstance(cursup,ActivePlayerSupply):
+            tb = sys.exc_info()[2]
+            errmsg = 'PlayerSupply.sync() expects argument of type ActivePlayerSupply.'
+            raise TypeError(errmsg).with_traceback(tb)
+#         print('k we got here')
+        for attr in ['production','units','structures']:
+#             print(f'  attr is {attr}')
+            current = cursup.__getattribute__(attr)
+            record = self.__getattribute__(attr)
+            d = {}
+#             print(f'current is {current}')
+#             print(current.values())
+#             print('  goin into loop')
+            for unit in current.values():
+#                 print(f'    unit is {unit}')
+                key = (unit.name,unit)
+                d[key] = d.get(key,0)+1
+#             print(' finished looping over current vals')
+            if (record[-1][0]==frame):
+                record.pop()
+                record.append((frame,d))
+            elif (record[-1][1] != d):
+                record.append((frame,d))
+        # loop over record to sum up supply
+
+    @staticmethod
+    def _timestr_to_seconds(timestr,useGameTimeStr=True):
+        m,s = map(int,timestr.split(':'))
+        s += m*60
+        if useGameTimeStr:
+            s = s*1.4
+        return s
+        
+    @staticmethod
+    def _seconds_to_timestr(seconds,useGameTimeStr=True):
+            s = seconds
+            if useGameTimeStr:
+                s = s/1.4
+            m = s/60.0
+            return f'{int(m)}:{int((m-int(m))*60.0):02}'
+
+    def time_supply_capped(self,useGameTime=True):
+        return self._seconds_to_timestr(self.supply_capped_real_seconds,useGameTime)    
+    
+    def workers(self):
+        return [(d[0],d[1]) for d in self.data]
+    
+    def army(self):
+        return [(d[0],d[2]) for d in self.data]
+    
+    def supply(self):
+        return [(d[0],d[1]+d[2]) for d in self.data]
+    
+    def capacity(self):
+        return [(d[0],d[3]) for d in self.data]    
+    
+    
 class SupplyTracker(object):
-    def add_to_units_alive(self, event, replay):
-        unit_name = event.unit_type_name
-        if unit_name in self.unit_name_to_supply:
-            supplyCount = self.unit_name_to_supply[event.unit_type_name][0]
-            buildTime = self.unit_name_to_supply[event.unit_type_name][1]
-            time_built = event.second - buildTime
-            time_built = 0 if time_built < 0 else time_built
-            new_unit = (supplyCount, event.unit_id)
-            self.units_alive[event.control_pid].append(new_unit)
-            total_supply = sum([x[0] for x in self.units_alive[event.control_pid]])
-            replay.players[event.control_pid - 1].current_food_used[
-                time_built
-            ] = total_supply
-            print(
-                "Second",
-                time_built,
-                replay.players[event.control_pid - 1],
-                "SUPPLY",
-                replay.players[event.control_pid - 1].current_food_used[time_built],
-            )
 
-        elif unit_name in self.supply_gen_unit:
-            ## see if the unit provides supply
-            supply_gen_count = self.supply_gen_unit[event.unit_type_name][0]
-            build_time = self.supply_gen_unit[event.unit_type_name][1]
-            time_complete = event.second + build_time
-            supply_gen_unit = (supply_gen_count, event.unit_id)
-            self.supply_gen[event.control_pid].append(supply_gen_unit)
-            total_supply_gen = sum([x[0] for x in self.supply_gen[event.control_pid]])
-            replay.players[event.control_pid - 1].current_food_made[
-                time_complete
-            ] = total_supply_gen
-            print(
-                "Second",
-                time_complete,
-                replay.players[event.control_pid - 1],
-                "Built",
-                replay.players[event.control_pid - 1].current_food_made[time_complete],
-            )
-        else:
-            print("Unit name {0} does not exist".format(event.unit_type_name))
-        return
+    name = "SupplyTracker"
 
-    def remove_from_units_alive(self, event, replay):
-        died_unit_id = event.unit_id
-        for player in replay.player:
-            dead_unit = filter(lambda x: x[1] == died_unit_id, self.units_alive[player])
-            if dead_unit:
-                self.units_alive[player].remove(dead_unit[0])
-                total_supply = sum([x[0] for x in self.units_alive[player]])
-
-                replay.players[player - 1].current_food_used[
-                    event.second
-                ] = total_supply
-                print(
-                    "Second",
-                    event.second,
-                    "Killed",
-                    event.unit.name,
-                    "SUPPLY",
-                    replay.players[player - 1].current_food_used[event.second],
-                )
-
-            dead_supply_gen = filter(
-                lambda x: x[1] == died_unit_id, self.supply_gen[player]
-            )
-            if dead_supply_gen:
-                self.supply_gen[player].remove(dead_supply_gen[0])
-                total_supply_gen = sum([x[0] for x in self.supply_gen[player]])
-                replay.players[player - 1].current_food_made[
-                    event.second
-                ] = total_supply_gen
-                print(
-                    "Second",
-                    event.second,
-                    "Killed",
-                    event.unit.name,
-                    "SUPPLY",
-                    replay.players[player - 1].current_food_made[event.second],
-                )
-
+    def unwrapUnitEvent(self,event):
+        try:
+            frame = event.frame
+            unit = event.unit._type_class
+            uid = event.unit_id
+            if event.unit.owner:
+                pid = event.unit.owner.pid
+                acs = self.active_supply[pid]
+            else:
+                pid = None
+                acs = None
+            return (frame,unit,uid,pid,acs)
+        except:
+            print(f'Unhandled UnitEvent {event}.')
+            raise
+                
     def handleInitGame(self, event, replay):
-        ## This dictionary contains the supply of every unit
-        self.unit_name_to_supply = {
-            # Zerg
-            "Drone": (1, 17),
-            "Zergling": (1, 25),
-            "Baneling": (0, 20),
-            "Queen": (2, 50),
-            "Hydralisk": (2, 33),
-            "Roach": (2, 27),
-            "Infestor": (2, 50),
-            "Mutalisk": (2, 33),
-            "Corruptor": (2, 40),
-            "Utralisk": (6, 55),
-            "Broodlord": (2, 34),
-            "SwarmHost": (3, 40),
-            "Viper": (3, 40),
-            # Terran
-            "SCV": (1, 17),
-            "Marine": (1, 25),
-            "Marauder": (2, 30),
-            "SiegeTank": (2, 45),
-            "Reaper": (1, 45),
-            "Ghost": (2, 40),
-            "Hellion": (2, 30),
-            "Thor": (6, 60),
-            "Viking": (2, 42),
-            "Medivac": (2, 42),
-            "Raven": (2, 60),
-            "Banshee": (3, 60),
-            "Battlecruiser": (6, 90),
-            "Hellbat": (2, 30),
-            "WidowMine": (2, 40),
-            # Protoss
-            "Probe": (1, 17),
-            "Zealot": (2, 38),
-            "Stalker": (2, 42),
-            "Sentry": (2, 42),
-            "Observer": (1, 30),
-            "Immortal": (4, 55),
-            "WarpPrism": (2, 50),
-            "Colossus": (6, 75),
-            "Phoenix": (2, 35),
-            "VoidRay": (4, 60),
-            "HighTemplar": (2, 55),
-            "DarkTemplar": (2, 55),
-            "Archon": (4, 12),
-            "Carrier": (6, 120),
-            "Mothership": (6, 100),
-            "MothershipCore": (2, 30),
-            "Oracle": (3, 50),
-            "Tempest": (4, 60),
-        }
+        try:
+            ## This dictionary contains the supply of every unit
+            self.supply_units = self.getUnitSupplyData()
+            self.supply = dict()                 
+            self.active_supply = dict()
 
-        self.supply_gen_unit = {
-            # overlord build time is zero because event for units are made when
-            # it is born not when it's created
-            "Overlord": (8, 0),
-            "Hatchery": (2, 100),
-            "SupplyDepot": (8, 30),
-            "CommandCenter": (11, 100),
-            "Pylon": (8, 25),
-            "Nexus": (10, 100),
-        }
-        ## This list contains a turple of the units supply and unit ID.
-        ## the purpose of the list is to know which user owns which unit
-        ## so that when a unit dies, that
-        self.units_alive = dict()
-        ##
-        self.supply_gen = dict()
-        for player in replay.players:
-            self.supply_gen[player.pid] = list()
-            self.units_alive[player.pid] = list()
-            player.current_food_used = defaultdict(int)
-            player.current_food_made = defaultdict(int)
-            player.time_supply_capped = int()
+            for player in replay.players:
+                self.supply[player.pid] = PlayerSupply()
+                self.active_supply[player.pid] = ActivePlayerSupply()
 
+            ## This list contains a tuple of the units supply and unit ID.
+            ## the purpose of the list is to know which user owns which unit
+            ## so that when a unit dies, that
+            self.units_alive = dict()
+            ##
+            self.supply_gen = dict()
+
+            for player in replay.players:
+                self.supply_gen[player.pid] = list()
+                self.units_alive[player.pid] = list()
+                #player.supply = PlayerSupply()
+                player.current_food_used = defaultdict(int)
+                player.current_food_made = defaultdict(int)
+                player.time_supply_capped = int()
+        except:
+            print('Error in SupplyTracker handleInitGame(...)')
+
+    def handleUnitTypeChangeEvent(self,event,replay):
+        try:
+            f,unit,uid,pid,acs = self.unwrapUnitEvent(event)
+            if event.unit_type_name=="Egg":
+                # spend larva from units and put egg in preprod queue
+                # where it waits for a corresponding "Morph" event
+                acs.units.pop(uid)
+                acs.preproduction.setdefault(f,[]).append(uid)
+            elif event.unit_type_name=="Larva":
+                # remove egg from production and add larva back to units
+                acs.production.pop(uid, None) # this shouldn't need to be none probably, diagnose
+                acs.units[uid]=unit
+            # are there more UnitTypeChangeEvents for other unit morphs (lurker,broods)?
+            self.supply[pid].sync(f,acs)
+        except:
+            print(f'Unhandled UnitTypeChangeEvent : {event}')
+        
+    def handleBasicCommandEvent(self, event, replay):
+        try:
+            f = event.frame
+            pid = event.player.pid
+            if event.has_ability:
+                acs = self.active_supply[pid]
+                if event.ability.name.startswith('MorphTo'):
+                    pass # handle ravagers, lurkers, broods, banes, ovies
+                elif event.ability.name.startswith('Morph'):
+                    unit = event.ability.build_unit
+                    preprod = acs.preproduction
+                    larva_id = acs.preproduction[f].pop(0)
+                    if not acs.preproduction[f]:
+                        acs.preproduction.pop(f)
+                    acs.production[larva_id]=unit
+                self.supply[pid].sync(f,acs)
+            # may need to handle Terran and Protoss events here as well
+        except:
+            print(f'Unhandled BasicCommandEvent : {event}')
+                  
+            
     def handleUnitInitEvent(self, event, replay):
-        # print ("Init",event.unit_type_name, event.unit_id)
-        self.add_to_units_alive(event, replay)
+        try:
+            f,unit,uid,pid,acs = self.unwrapUnitEvent(event)
+            acs.production[uid]=unit
+            self.supply[pid].sync(f,acs)
+        except:
+            print(f'Unhandled UnitInitEvent : {event}')
+        
+    def handleUnitDoneEvent(self, event, replay):
+        try:
+            f,unit,uid,pid,acs = self.unwrapUnitEvent(event)
+            acs.production.pop(uid)
+            acs.structures[uid] = unit
+            self.supply[pid].sync(f,acs)
+        except:
+            print(f'Unhandled UnitDoneEvent : {event}')
 
     def handleUnitBornEvent(self, event, replay):
-        # print ("Born",event.unit_type_name,event.unit_id)
-        self.add_to_units_alive(event, replay)
-
+        try:
+            f,unit,uid,pid,acs = self.unwrapUnitEvent(event)
+            if pid:
+                if not unit.name.startswith('Beacon'):
+                    if unit.is_building:
+                        acs.structures[uid]=unit
+                    else:
+                        acs.units[uid] = unit
+                    self.supply[pid].sync(f,acs)
+        except:
+            print(f'Unhandled UnitBornEvent : {event}')
+            
     def handleUnitDiedEvent(self, event, replay):
-        if event.unit.name not in self.unit_name_to_supply:
-            return
-        self.remove_from_units_alive(event, replay)
+        try:
+            f,unit,uid,pid,acs = self.unwrapUnitEvent(event)
+            if pid:
+                if not unit.name.startswith('Beacon'):
+                    acs.units.pop(uid,None)
+                    acs.production.pop(uid,None)
+                    acs.structures.pop(uid,None)
+                    self.supply[pid].sync(f,acs)
+        except:
+            print(f'Unhandled UnitDiedEvent : {event}')
 
     def handleEndGame(self, event, replay):
         for player in replay.players:
-            player.current_food_used = sorted(
-                player.current_food_used.iteritems(), key=lambda x: x[0]
-            )
-            player.current_food_made = sorted(
-                player.current_food_made.iteritems(), key=lambda x: x[0]
-            )
+            player.foo = 'bar'
+            # need to do any sorting?
+            player.supply = self.supply[player.pid]
+            player.current_food_used = dict(sorted(player.current_food_used.items()))
+            player.current_food_made = dict(sorted(player.current_food_made.items()))
+            #player.supply.data.sort(key=lambda x: x[0])
+        print('end of game!')
+            
+    def getUnitSupplyData(self):
+        return {
+            ### Zerg ### For the swarm!
+            "Drone": UnitSupply(17, 1, SupplyType.WORKER),
+            "Zergling": UnitSupply(25, 0.5, 25),
+            "Baneling": UnitSupply(20, 0),
+            "Queen": UnitSupply(50, 2),
+            "Hydralisk": UnitSupply(33, 2),
+            "Roach": UnitSupply(27, 2),
+            "Infestor": UnitSupply(50, 2),
+            "Mutalisk": UnitSupply(33, 2),
+            "Corruptor": UnitSupply(40, 2),
+            "Ultralisk": UnitSupply(55, 6),
+            "Broodlord": UnitSupply(34, 2),
+            "SwarmHost": UnitSupply(40, 3),
+            "Viper": UnitSupply(40, 3),
+            "Overlord": UnitSupply(0, 8, SupplyType.CAPACITY),
+            "Hatchery": UnitSupply(100, 6, SupplyType.CAPACITY),
+            # Terran
+            "SCV": UnitSupply(17, 1, SupplyType.WORKER),
+            "Marine": UnitSupply(25, 1),
+            "Marauder": UnitSupply(30, 2),
+            "SiegeTank": UnitSupply(45, 2),
+            "Reaper": UnitSupply(45, 1),
+            "Ghost": UnitSupply(40, 2),
+            "Hellion": UnitSupply(30, 2),
+            "Thor": UnitSupply(60, 6),
+            "Viking": UnitSupply(42, 2),
+            "Medivac": UnitSupply(42, 2),
+            "Raven": UnitSupply(60, 2),
+            "Banshee": UnitSupply(60, 3),
+            "Battlecruiser": UnitSupply(90, 6),
+            "BattleHellion": UnitSupply(30, 2),
+            "WidowMine": UnitSupply(40, 2),
+            "SupplyDepot": UnitSupply(30, 8, SupplyType.CAPACITY),
+            "CommandCenter": UnitSupply(100, 15, SupplyType.CAPACITY),
+            # Protoss
+            "Probe": UnitSupply(17, 1, SupplyType.WORKER),
+            "Zealot": UnitSupply(38, 2),
+            "Stalker": UnitSupply(42, 2),
+            "Sentry": UnitSupply(42, 2),
+            "Observer": UnitSupply(30, 1),
+            "Immortal": UnitSupply(55, 4),
+            "WarpPrism": UnitSupply(50, 2),
+            "Colossus": UnitSupply(75, 6),
+            "Phoenix": UnitSupply(35, 2),
+            "VoidRay": UnitSupply(60, 4),
+            "HighTemplar": UnitSupply(55, 2),
+            "DarkTemplar": UnitSupply(55, 2),
+            "Archon": UnitSupply(12, 4),
+            "Carrier": UnitSupply(120, 6),
+            "Mothership": UnitSupply(100, 6),
+            "MothershipCore": UnitSupply(30, 2),
+            "Oracle": UnitSupply(50, 3),
+            "Tempest": UnitSupply(60, 4),
+            "Pylon": UnitSupply(25, 8, SupplyType.CAPACITY),
+            "Nexus": UnitSupply(100, 15, SupplyType.CAPACITY)
+        }
